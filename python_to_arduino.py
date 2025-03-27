b8c9f2247f972a5c475abc44df32b414acf59284
Krithin Bheemineni <kbheem@umich.edu>
2025-03-27 10:47:59 -0400

    arduino/python code

---
+++ python_to_arduino.py
@@ -0,0 +1,18 @@
+import serial
+import time
+
+def send_frustration_value(frustration_value, port='/dev/tty.usbmodem1101', baudrate=9600):
+    try:
+        ser = serial.Serial(port, baudrate, timeout=1)
+        time.sleep(2)
+
+        ser.write(str(frustration_value).encode())
+        print(f"Sent frustration value: {frustration_value}")
+        ser.close()
+    except Exception as e:
+        print("Error communicating with Arduino:", e)
+
+if __name__ == "__main__":
+ 
+    frustration_value = 2 
+    send_frustration_value(frustration_value, port='/dev/tty.usbmodem1101', baudrate=9600)
